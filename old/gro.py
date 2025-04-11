2f0c888ec94cebf0250d6863e00dc3239c8cd0d9
Sindhuraja1613 <159508610+Sindhuraja1613@users.noreply.github.com>
2025-04-11 21:04:46 +0530

    Delete old directory

--- old/gro.py
+++
@@ -1,6 +0,0 @@
-from groq import Groq
-client = Groq(api_key="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")
-response = client.chat.completions.create(
-    model="llama3-70b-8192",
-    messages=[{"role": "user", "content": "What is Groq?"}]
-)
